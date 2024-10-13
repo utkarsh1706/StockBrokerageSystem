c1ef84270a00019e37e2565f594d4e6e85a661d9
Utkarsh Kuchhal <kuchhalutkarsh17@gmail.com>
2024-10-13 19:51:56 +0530

    Limiter Added

--- constants.py
+++ constants.py
@@ -11,4 +11,4 @@ lowerCircuitValid = False
 upperCircuitValid = False
 
 redisHost = 'redis-16083.c305.ap-south-1-1.ec2.redns.redis-cloud.com'
-storageRateLimit = "redis://redis-16083.c305.ap-south-1-1.ec2.redns.redis-cloud.com:16083"
+storageRateLimit = "redis://:k9Av3yeg0I5pIRixxWbMna2hwXQD0rnv@redis-16083.c305.ap-south-1-1.ec2.redns.redis-cloud.com:16083"
